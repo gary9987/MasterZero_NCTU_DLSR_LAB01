f31aa684b1efc41fb8e39f3df9a98b0bf5770ec1
Gary <s310068@shsh.tw>
2021-04-26 23:29:12 +0800

    add TOP3 acc in eval.py

--- eval.py
+++ eval.py
@@ -4,6 +4,35 @@ import torch
 from tqdm import tqdm
 from model import Net
 
+def evaluteTopK(k, model, loader):
+    model.eval()
+
+    class_correct = [0. for i in range(11)]
+    class_total = [0. for i in range(11)]
+
+    for images, labels in tqdm(loader):
+        images, labels = images.to(device), labels.to(device)
+
+        with torch.no_grad():
+            outputs = model(images)
+            y_resize = labels.view(-1, 1)
+            _, predicted = outputs.topk(k, 1, True, True)
+
+            for i in range(len(predicted)):
+                class_total[labels[i]] += 1
+                #print(torch.eq(predicted[i], y_resize[i]).sum().float().item())
+                class_correct[labels[i]] += torch.eq(predicted[i], y_resize[i]).sum().float().item()
+
+    for i in range(11):
+        print('Top %d Accuracy of class %2d is %3d/%3d  %.2f%%' % (
+            k, i, class_correct[i], class_total[i], (100 * class_correct[i] / class_total[i])))
+
+    print('Top %d accuracy of the network on the %d test images: %d/%d  %.2f %%'
+          % (k, sum(class_total), sum(class_correct), sum(class_total), (100 * sum(class_correct) / sum(class_total))))
+
+    return 100 * sum(class_correct) / sum(class_total)
+
+
 if __name__ == '__main__':
 
     transform_train = transforms.Compose([
@@ -14,7 +43,7 @@ if __name__ == '__main__':
     ])
 
     evalset = datasets.ImageFolder(root='food11re/evaluation/', transform=transform_train)
-    evalloader = DataLoader(dataset=evalset, batch_size=64)
+    evalloader = DataLoader(dataset=evalset, batch_size=100)
 
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
     net = Net()
@@ -22,22 +51,6 @@ if __name__ == '__main__':
     net = net.to(device)
     net.eval()
 
-    class_correct = [0. for i in range(11)]
-    class_total = [0. for i in range(11)]
+    print(evaluteTopK(1, net, evalloader))
+    print(evaluteTopK(3, net, evalloader))
 
-    with torch.no_grad():
-        for images, labels in tqdm(evalloader):
-            images = images.to(device)
-            labels = labels.to(device)
-            outputs = net(images)
-            _, predicted = outputs.max(1)
-            for i in range(len(predicted)):
-                class_total[labels[i]] += 1
-                class_correct[labels[i]] += predicted[i].eq(labels[i]).item()
-
-    for i in range(11):
-        print('Accuracy of class %2d is %3d/%3d  %.2f%%' % (
-        i, class_correct[i], class_total[i], (100 * class_correct[i] / class_total[i])))
-
-    print('Accuracy of the network on the %d test images: %d/%d  %.2f %%'
-          % (sum(class_total), sum(class_correct), sum(class_total), (100 * sum(class_correct) / sum(class_total))))
